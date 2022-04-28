3dcfc6a7fff195593db7d1a9094f6b01114b3a4d
Adam Palo <xpalo@stuba.sk>
2022-04-28 20:45:49 +0200

    Adam Initial commit

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {useTranslation} from 'react-i18next';
-import '../../../config/i18n';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { useTranslation } from "react-i18next";
+import "../../../config/i18n";
 
 function Example() {
-   const {t} = useTranslation()
+    const { t } = useTranslation();
     return (
         <div className="container">
             <div className="row justify-content-center">
                 <div className="col-md-8">
                     <div className="card">
-                        <div className="card-header text-white">{t('INIT')}</div>
+                        <div className="card-header text-white">
+                            {t("INIT")}
+                        </div>
+                        <div>NAZDÁREK</div>
                     </div>
                 </div>
             </div>
@@ -21,6 +23,6 @@ function Example() {
 
 export default Example;
 
-if (document.getElementById('example')) {
-    ReactDOM.render(<Example />, document.getElementById('example'));
+if (document.getElementById("example")) {
+    ReactDOM.render(<Example />, document.getElementById("example"));
 }
