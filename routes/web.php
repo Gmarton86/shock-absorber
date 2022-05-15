0afd4677b6c4d9f477fea2a8d1195d359e1566b1
matejkoval <kovalmatej00@gmail.com>
2022-05-15 14:38:52 +0200

    Created API routes for Octave calculations

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OctaveController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/calculate', function() {
+    return view("welcome");
+});
+
+Route::post('/calculate', [OctaveController::class, 'calculate'])->name("calculate");
+
+Route::get('/cmd', function() {
+    return view("welcome");
+});
+
+Route::post('/cmd', [OctaveController::class, 'command'])->name("cmd");
+
